92aa1e8866db2a40b23b976cfd2660a7802d8b25
asqiir <asqiir@blaupost.de>
2019-05-25 14:24:38 +0200

    header for control method

---
+++ control.py
@@ -0,0 +1,8 @@
+def move(power, directory, height):
+	'''Move the airship.
+		* power: integer 0 - 1023 (power of the stronger motor)
+		* directory: integer -127 - +128 (diff to 0 indicates ration of power of stronger and weaker motor.
+										 (when directory < 0 -> move left -> right motor)
+		* height: integer -127 - +128 (goes down when < 0)'''
+
+	pass
