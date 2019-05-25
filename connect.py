59eace3d3045ead08df57cd85270a30c116b1997
astoorangi <astoorangi@outlook.com>
2019-05-25 15:48:53 +0200

    connect

--- connect.py
+++ connect.py
@@ -5,5 +5,6 @@ def openAP(apname):
 	ap = network.WLAN(network.AP_IF) # create access-point interface
 	ap.config(essid=apname,authmode=network.AUTH_WPA_WPA2_PSK, password="PSK_Milus-AP") # set the ESSID of the access point
 	ap.active(True)         # activate the interface
+	return(ap)
 
-ap = openAP('Milus-AP')
+#ap = openAP('Milus-AP')
