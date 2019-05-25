18fc75bab9c1cd7ad5d11ae6ac50ea1ef2af6533
asqiir <asqiir@blaupost.de>
2019-05-25 16:08:13 +0200

    bugfix 2

--- motors.py
+++ motors.py
@@ -1,5 +1,6 @@
 from pins import * 
 import machine
+import math
 
 #ungesetzt
 #pwm_left_motor, pwm_right_motor, pwm_up_motor, pwm_down_motor = (False,False,False,False)
@@ -20,14 +21,15 @@ def setup():
 	pwm_up_motor.freq(2000)
 	pwm_down_motor.freq(2000)
 
+	set_power(0,0,0,0)
 
 def set_power(left, right, height, up):
-	pwm_left_motor.duty(left)
-	pwm_right_motor.duty(right)
+	pwm_left_motor.duty(math.floor(left))
+	pwm_right_motor.duty(math.floor(right))
 
 	if(up):
 		pwm_down_motor.duty(0)
-		pwm_up_motor.duty(height)
+		pwm_up_motor.duty(math.floor(height))
 	else:
 		pwm_up_motor.duty(0)
-		pwm_down_motor.duty(height)
+		pwm_down_motor.duty(math.floor(height))
